25a0965d272f80e59964e6e3ccf8952c1ad95618
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2009-09-11 07:40:30 +0000

    Fix modifiers and add more instructions

--- test-suite/gas/format_type_RI.s
+++ test-suite/gas/format_type_RI.s
@@ -2,3 +2,14 @@
 loop:
   cmp16 %g0, 0xff
   ecmp16 %g0, 0xddff
+  cmp16 %g2, -1
+  cmp16 %g0, -128
+  cmp16 %g0, 127
+  ecmp16 %g0, -262144 # -2^18
+  ecmp16 %g0, 262143 # 2^18 - 1
+  ecmp16 %g0, -1
+  mov16 0xff, %g0
+  mov16 -1, %g0
+  emov16 0x7ffff, %g0
+  esethi16 loop+3, %g0 
+  esethi16 %hi(loop), %g0
