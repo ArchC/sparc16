eee56c04c433fa4a6ebd8480cc525bcdc67fb757
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2010-02-04 22:14:16 +0000

    Move sethi patterns around, delete some instructions from testcase and fix encoding/decoding of sethi immediates

--- test-suite/gas/format_type_RI.s
+++ test-suite/gas/format_type_RI.s
@@ -1,5 +1,3 @@
-# TODO: esethi support for %gdop_hix22/lowx10
-
 loop:
   cmp16 %i1, 0xff
   ecmp16 %i1, 0x1fff
@@ -12,6 +10,6 @@ loop:
   mov16 0xff, %i1
   mov16 -1, %i1
   emov16 0x7ffff, %i1
-  esethi16 %hi(0x7ffff000), %i1
-  esethi16 loop+3, %i1 
+  esethi16 0x7ffff, %i1 # high part of 0xffffe000
+  esethi16 %hi(0xffffe000), %i1
   esethi16 %hi(loop), %i1
