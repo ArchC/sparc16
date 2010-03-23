74a26cd61adbfc259d0bad32d9fb175e69ad8398
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2010-03-23 22:51:24 +0000

    add a relocation for mov instructions

--- toolchain-scripts/compile-binutils.sh
+++ toolchain-scripts/compile-binutils.sh
@@ -4,4 +4,4 @@
 #tar xjf /p/toolchains/srcs/binutils-2.15.tar.bz2 -C \
 #  /p/toolchains/srcs/
 cd /p/archc-tools/sparc16 && rm -rf acbingenbuilddir
-rm -f /usr/local/sparc16/* && acbingen.sh -i/usr/local/sparc16 sparc16.ac
+sudo rm -f /usr/local/sparc16/* && acbingen.sh -i/usr/local/sparc16 sparc16.ac
