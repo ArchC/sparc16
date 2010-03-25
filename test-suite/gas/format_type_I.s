cdc2998ed98343f46521d2b892fc68e416029d90
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2010-03-25 18:20:54 +0000

    add annul mode for be, b, bde

--- test-suite/gas/format_type_I.s
+++ test-suite/gas/format_type_I.s
@@ -8,8 +8,16 @@ loop:
   eb16 loop
   be16 loop
   ebe16 loop
-  call16 loop
-  ecall16 loop
   bne16 loop
   ebne16 loop
+
+  #b16,a loop
+  #eb16,a loop
+  #be16,a loop
+  #ebe16,a loop
+  #bne16,a loop
+  #ebne16,a loop
+
+  call16 loop
+  ecall16 loop
   callx loop
--- toolchain-scripts/compile-binutils.sh
+++ toolchain-scripts/compile-binutils.sh
@@ -4,4 +4,4 @@
 #tar xjf /p/toolchains/srcs/binutils-2.15.tar.bz2 -C \
 #  /p/toolchains/srcs/
 cd /p/archc-tools/sparc16 && rm -rf acbingenbuilddir
-sudo rm -f /usr/local/sparc16/* && acbingen.sh -i/usr/local/sparc16 sparc16.ac
+rm -f /usr/local/sparc16/* && acbingen.sh -i/usr/local/sparc16 sparc16.ac
